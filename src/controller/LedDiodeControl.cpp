909cfd9cd6220d7be7c5a6c7e4de1ca292e193af
Artur Pietrzyk <artudi54@gmail.com>
2019-10-24 00:02:03 +0200

    Switched to HAL from standard peripheral library

--- src/controller/LedDiodeControl.cpp
+++ src/controller/LedDiodeControl.cpp
@@ -1,20 +1,21 @@
 #include "LedDiodeControl.hpp"
 #include <cstdint>
 #include <stdexcept>
-#include <stm32f4xx_gpio.h>
-#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx_hal_gpio.h>
+#include <stm32f4xx_hal_rcc.h>
 
 namespace controller {
-    static std::uint16_t getPin(LedDiodeControl::Color color) {
+    static std::uint16_t getPIN(LedDiodeControl::Color color) {
         switch (color)  {
             case LedDiodeControl::Color::Green:
-                return GPIO_Pin_12;
+                return GPIO_PIN_12;
             case LedDiodeControl::Color::Orange:
-                return GPIO_Pin_13;
+                return GPIO_PIN_13;
             case LedDiodeControl::Color::Red:
-                return GPIO_Pin_14;
+                return GPIO_PIN_14;
             case LedDiodeControl::Color::Blue:
-                return GPIO_Pin_15;
+                return GPIO_PIN_15;
             default:
                 throw std::invalid_argument("Invalid diode color");
         }
@@ -26,21 +27,21 @@ namespace controller {
     }
 
     void LedDiodeControl::enableDiode(LedDiodeControl::Color color) {
-        GPIO_WriteBit(GPIOD, getPin(color), Bit_SET);
+        HAL_GPIO_WritePin(GPIOD, getPIN(color), GPIO_PIN_SET);
     }
 
     void LedDiodeControl::disableDiode(LedDiodeControl::Color color) {
-        GPIO_WriteBit(GPIOD, getPin(color), Bit_RESET);
+        HAL_GPIO_WritePin(GPIOD, getPIN(color), GPIO_PIN_RESET);
     }
 
     LedDiodeControl::LedDiodeControl() {
-        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+		HAL_Init();
+        __HAL_RCC_GPIOD_CLK_ENABLE();
         GPIO_InitTypeDef gpio;
-        gpio.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-        gpio.GPIO_Mode = GPIO_Mode_OUT;
-        gpio.GPIO_Speed = GPIO_High_Speed;
-        gpio.GPIO_OType = GPIO_OType_PP;
-        gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
-        GPIO_Init(GPIOD, &gpio);
+        gpio.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+        gpio.Mode = GPIO_MODE_OUTPUT_PP;
+        gpio.Pull = GPIO_NOPULL;
+        gpio.Speed = GPIO_SPEED_FREQ_LOW;
+        HAL_GPIO_Init(GPIOD, &gpio);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
-
+#include <controller/LedDiodeControl.hpp>
 extern "C" int main() {
+	controller::LedDiodeControl& control = controller::LedDiodeControl::getInstance();
+	control.enableDiode(controller::LedDiodeControl::Color::Red);
+	control.enableDiode(controller::LedDiodeControl::Color::Green);
     while (true);
     return 0;
 }      
