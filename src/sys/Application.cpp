80fb5cf3b6a2b733098400f3c9a75c3496c01450
Artur Pietrzyk <artudi54@gmail.com>
2019-11-11 23:32:15 +0100

    Fixed slow playing problem

--- src/sys/Application.cpp
+++ src/sys/Application.cpp
@@ -64,31 +64,44 @@ namespace sys {
     void Application::initializeClock() {
         RCC_OscInitTypeDef RCC_OscInitStruct = {};
         RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+        RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {};
 
         __HAL_RCC_PWR_CLK_ENABLE();
         __HAL_RCC_SYSCFG_CLK_ENABLE();
         __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
+        /** Initializes the CPU, AHB and APB busses clocks
+        */
         RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
         RCC_OscInitStruct.HSEState = RCC_HSE_ON;
         RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
         RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-        RCC_OscInitStruct.PLL.PLLM = 25;
-        RCC_OscInitStruct.PLL.PLLN = 144;
+        RCC_OscInitStruct.PLL.PLLM = 4;
+        RCC_OscInitStruct.PLL.PLLN = 168;
         RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
         RCC_OscInitStruct.PLL.PLLQ = 4;
-        if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+        {
             //TODO
         }
-
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+        /** Initializes the CPU, AHB and APB busses clocks
+        */
+        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                      |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
         RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
         RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+        {
+            //TODO
+        }
+        PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
+        PeriphClkInitStruct.PLLI2S.PLLI2SN = 50;
+        PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
+        if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+        {
             //TODO
         }
     }
