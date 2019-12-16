15ad4e03da6707a28d1509461a156863d93d8082
Artur Pietrzyk <artudi54@gmail.com>
2019-12-16 23:17:09 +0100

    Added mocks for request handler

--- include/service/RequestHandler.hpp
+++ include/service/RequestHandler.hpp
@@ -1,18 +1,29 @@
 #pragma once
 #include <array>
+#include <player_protocol/MessageVisitor.hpp>
 #include <audio/PlaylistPlayer.hpp>
 #include <sys/BluetoothCommunicationProvider.hpp>
 #include <sys/Task.hpp>
 
 namespace service {
-    class RequestHandler : public sys::Task {
+    class RequestHandler : private sys::Task , private player_protocol::MessageServerVisitor {
     public:
         RequestHandler(sys::BluetoothCommunicationProvider& communicationProvider, audio::PlaylistPlayer& player);
 
-        void progress() override;
     private:
-        void handlePlay();
-        void handlePause();
+        void progress() override;
+
+        void handleMessage(const player_protocol::request::ChangeMediumRequest& message) override;
+        void handleMessage(const player_protocol::request::ChangeVolumeRequest& message) override;
+        void handleMessage(const player_protocol::request::PauseRequest& message) override;
+        void handleMessage(const player_protocol::request::PlaylistRequest& message) override;
+        void handleMessage(const player_protocol::request::PlayRequest& message) override;
+        void handleMessage(const player_protocol::request::SeekRequest& message) override;
+        void handleMessage(const player_protocol::request::StopRequest& message) override;
+        void handleInvalidMessage(const player_protocol::Message &message) override;
+
+        void sendMessage(const player_protocol::Message& message);
+
         sys::BluetoothCommunicationProvider& communicationProvider;
         audio::PlaylistPlayer& player;
         std::array<char, 256> buffer;
---
+++ src/service/RequestHandler.cpp
@@ -0,0 +1,60 @@
+#include "RequestHandler.hpp"
+#include <player_protocol/MessageSerializer.hpp>
+#include <player_protocol/request/PlayRequest.hpp>
+#include <player_protocol/request/ChangeVolumeRequest.hpp>
+
+namespace service {
+    RequestHandler::RequestHandler(sys::BluetoothCommunicationProvider &communicationProvider, audio::PlaylistPlayer &player)
+        : communicationProvider(communicationProvider)
+        , player(player)
+        , buffer{} {}
+
+    void RequestHandler::progress() {
+        if (!communicationProvider.hasSizedMessage())
+            return;
+
+        communicationProvider.receiveSizedMessage(buffer.data());
+        auto message = player_protocol::MessageSerializer::deserialize(buffer.data());
+        message->visit(*this);
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::ChangeMediumRequest &message) {
+
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::ChangeVolumeRequest &message) {
+        try {
+            player.setVolume(message.newVolume);
+        }
+        catch (std::exception& exc) {
+        }
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::PauseRequest &message) {
+
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::PlaylistRequest &message) {
+
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::PlayRequest &message) {
+
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::SeekRequest &message) {
+
+    }
+
+    void RequestHandler::handleMessage(const player_protocol::request::StopRequest &message) {
+
+    }
+
+    void RequestHandler::handleInvalidMessage(const player_protocol::Message &message) {
+
+    }
+
+    void RequestHandler::sendMessage(const player_protocol::Message &message) {
+
+    }
+}
